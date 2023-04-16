a27c8d24d3730731cbe6b8749b8606a4ce4abf26
CDesiree <desireecastrocpe@gmail.com>
2023-04-16 19:55:15 +0800

    [fix] remove duplicate header

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,19 +1,12 @@
 import React from "react";
-import {Layout} from "antd";
-
-const {Header} = Layout;
 
 function Home() {
     return (
         <>
         <div className="App">
-            <Layout>
-                <Header>
-                    <div className="home-page">
-                        <a>Home page contents</a>
-                    </div>
-                </Header>
-            </Layout>
+            <div className="home-page">
+                <p>Home page contents</p>
+            </div>
         </div>
         </>
     );
