ea5ddb88e1dbb4769b28a9254778d4a0c8005999
CDesiree <desireecastrocpe@gmail.com>
2023-04-16 19:56:03 +0800

    [feat] arrange header display with hover effects

--- src/components/layout/SiteLayout.jsx
+++ src/components/layout/SiteLayout.jsx
@@ -1,20 +1,44 @@
-import { Button, Layout } from "antd";
+import { Button, ConfigProvider, Layout } from "antd";
 import React from "react";
 import { Content, Header } from "antd/lib/layout/layout";
 import { Outlet } from "react-router-dom";
-import { YoutubeOutlined, FacebookOutlined } from '@ant-design/icons';
+import { YoutubeOutlined, FacebookOutlined, TwitterOutlined } from '@ant-design/icons';
 import "./SiteLayout.css";
+import logo from '../assets/kessoku-band-logo.webp';
 
 function SiteLayout() {
 
     return (
            <Layout>
                <Header className="header">
-                   <div className="left-btn">
-                        <Button className="logo-btn" type="text" href="/">Logo</Button>
-                        <Button className="tour-btn" type="text" href="/tour">Tour</Button>
-                        <Button className="merch-btn" type="text" href="/merch">Merch</Button>
-                        <Button className="music-btn" type="text" href="/music">Music</Button>
+                    <div className="left-btn">
+                        <div className="logo-cont">
+                            <Button  className="logo-btn"
+                                 type="text"
+                                 href="/">
+                                <img src={logo} className="band-logo" alt="logo"/>
+                            </Button>
+                        </div>
+                        <ConfigProvider
+                            theme={{
+                                token: {
+                                    colorBgTextHover: 'rgb(255, 0, 79)',
+                                },
+                            }}>
+                        <div className="menu-cont">
+                            <Button className="tour-btn" type="text" href="/tour">TOUR</Button>
+                            <Button className="merch-btn" type="text" href="/merch">MERCH</Button>
+                            <Button className="music-btn" type="text" href="/music">MUSIC</Button>
+                        </div>
+                        </ConfigProvider>
+                    </div>
+                    <div className="right-btn">
+                        <ConfigProvider
+                            theme={{
+                                token: {
+                                    colorBgTextHover: 'rgb(255, 0, 79)',
+                                },
+                            }}>
                         <Button className="fb-btn" 
                                 type="text"
                                 href="https://www.aniplus-asia.com/show/bocchi-the-rock/?fbclid=IwAR2RzWi7pHPWAx3xXWE1y7__K9QalZ48UfeF_0zVtd4ekZysQBv1Mvbxu8c">
@@ -25,6 +49,12 @@ function SiteLayout() {
                                 href="https://www.youtube.com/channel/UC6IhDHJbJUoRJGUPnlh5GRQ/featured">
                             <YoutubeOutlined />
                         </Button>
+                        <Button className="tw-btn" 
+                                type="text"
+                                href="https://twitter.com/AniTrendz/status/1605291949820256256?lang=en">
+                            <TwitterOutlined />
+                        </Button>
+                        </ConfigProvider>
                    </div>
                </Header>
                <Content className="site-layout-bg">
